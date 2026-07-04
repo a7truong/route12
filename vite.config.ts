658110e28dfecbc1ab2e15aa94ee1429053bfbfa
Andy Truong <andy.truong16@gmail.com>
2026-07-03 20:09:29 -0400

    Update links for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/route12/',
-})
+
+  // must be "/" when using a custom domain, if using the GitHub pages domain, change to "/route12/" - must also change homepage in package.json to the https://a7truong.github.io/route12 URL as well
+  base: "/",
+});
